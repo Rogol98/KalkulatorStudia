b61c022afd7de5f4e81ac2aedef9e0d49217933d
Rogol98 <rogkar98@gmail.com>
2020-03-23 15:19:41 +0100

    init commit

---
+++ Kalkulator/Kalkulator/MainWindow.xaml.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Kalkulator
+{
+    public partial class MainWindow : Window
+    {
+        OperacjeMatematyczne op = new OperacjeMatematyczne();
+        double wartosc = 0;
+      
+        string operacja = "";
+        
+        bool operacja_w = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            if (screenik.Text == "0" || operacja_w)
+            {
+                screenik.Clear();
+                operacja_w = false;
+            }
+            Button butt = (Button)sender;
+
+            if (butt.Content.ToString() == ",")
+            {
+                if (!screenik.Text.Contains(","))
+                {
+                    screenik.Text = screenik.Text + butt.Content.ToString();
+                }
+            }
+            else
+            {
+                screenik.Text = screenik.Text + butt.Content.ToString();
+            }
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            screenik.Clear();
+            wartosc = 0;
+            pamiec.Content = string.Empty;
+            screenik.Text = "0";
+        }
+
+        private void ZmianaZnakuLiczby(object sender, RoutedEventArgs e)
+        {
+            if (!Double.TryParse(screenik.Text, out wartosc))
+            {
+                wartosc = 0;
+            }
+            wartosc = wartosc * (-1);
+            screenik.Text = wartosc.ToString();
+        }
+
+        private void Op_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            operacja = b.Content.ToString();
+            if(!Double.TryParse(screenik.Text, out wartosc))
+            {
+                wartosc = 0;
+            }
+
+            operacja_w = true;
+
+       
+            pamiec.Content = wartosc + " " + operacja;
+        }
+
+   
+        private void Wynik(object sender, RoutedEventArgs e)
+        {
+            pamiec.Content = "";
+
+            switch (operacja)
+            {
+                case "+":
+                    screenik.Text = op.Dodaj(wartosc, screenik.Text);
+                    break;
+                case "-":
+                    screenik.Text = op.Odejmij(wartosc, screenik.Text);
+                    break;
+                case "*":
+                    screenik.Text = op.Mnozenie(wartosc, screenik.Text);
+                    break;
+                case "/":
+                    screenik.Text = op.Dzielenie(wartosc, screenik.Text);
+                    break;
+                case "^":
+                    screenik.Text = op.Potega(wartosc, screenik.Text);
+                    break;
+                case "%":
+                    screenik.Text = op.Procent(wartosc, screenik.Text);
+                    break;
+                case "√":
+                    screenik.Text = op.Pierwiastek(wartosc);
+                    break;
+                case "mod":
+                    screenik.Text = op.Modulo(wartosc, screenik.Text);
+                    break;
+                case "bin":
+                    screenik.Text = op.Binary(screenik.Text);
+                    break;
+                case "!":
+                    screenik.Text = op.Silnia(screenik.Text);
+                    break;
+                case "sin":
+                    screenik.Text = op.Sinus(screenik.Text);
+                    break;
+                case "cos":
+                    screenik.Text = op.Cosinus(screenik.Text);
+                    break;
+                case "tg":
+                    screenik.Text = op.Tangens(screenik.Text);
+                    break;
+                case "ctg":
+                    screenik.Text = op.Cotangens(screenik.Text);
+                    break;
+                case "sinh":
+                    screenik.Text = op.SinusH(screenik.Text);
+                    break;
+                case "cosh":
+                    screenik.Text = op.CosinusH(screenik.Text);
+                    break;
+                case "log":
+                    screenik.Text = op.Log10(screenik.Text);
+                    break;
+                case "ln":
+                    screenik.Text = op.Ln(screenik.Text);
+                    break;
+                default:
+                    break;
+            }
+
+            operacja_w = false;
+        }
+    }
+
+    public class OperacjeMatematyczne
+    {
+        public string Dodaj(double a, string b)
+        {
+            if (!Double.TryParse(b, out double c))
+            {
+                return a.ToString();
+            }
+
+            return (a + c).ToString();
+        }
+
+        public string Odejmij(double a, string b)
+        {
+            if (!Double.TryParse(b, out double c))
+            {
+                return a.ToString();
+            }
+
+            return (a - c).ToString();
+        }
+
+        public string Mnozenie(double a, string b)
+        {
+            if (!Double.TryParse(b, out double c))
+            {
+                return a.ToString();
+            }
+
+            return (a * c).ToString();
+        }
+
+        public string Dzielenie(double a, string b)
+        {
+            if (!Double.TryParse(b, out double c))
+            {
+                return a.ToString();
+            }
+
+            return (a/c).ToString();
+        }
+
+        public string Potega(double a, string b)
+        {
+            if (!Double.TryParse(b, out double c))
+            {
+                return a.ToString();
+            }
+
+            return Math.Pow(a, c).ToString();
+        }
+
+        public string Procent(double a, string b)
+        {
+            if (!Double.TryParse(b, out double c))
+            {
+                return a.ToString();
+            }
+
+            return (a / c).ToString("0.00%");
+        }
+
+        public string Pierwiastek(double a)
+        {
+            return Math.Sqrt(a).ToString();
+        }
+
+        public string Modulo(double a, string b)
+        {
+            if (!Double.TryParse(b, out double c))
+            {
+                return a.ToString();
+            }
+
+            return (a % c).ToString();
+        }
+
+        public string Binary(string a)
+        {
+            if (!int.TryParse(a, out int b))
+            {
+                b = 0;
+            }
+
+            return Convert.ToString(b, 2);
+        }
+
+        public string Silnia(string a)
+        {
+            if (!int.TryParse(a, out int b))
+            {
+                return "1";
+            }
+
+            int wynik = 1;
+
+            for(int i = 1; i <= b; ++i)
+            {
+                wynik *= i;
+            }
+
+            return wynik.ToString();
+        }
+
+        public string Sinus(string a)
+        {
+            if (!Double.TryParse(a, out double b))
+            {
+                return a.ToString();
+            }
+
+            return Math.Sin(b).ToString();
+        }
+
+        public string Cosinus(string a)
+        {
+            if (!Double.TryParse(a, out double b))
+            {
+                return a.ToString();
+            }
+
+            return Math.Cos(b).ToString();
+        }
+
+        public string Tangens(string a)
+        {
+            if (!Double.TryParse(a, out double b))
+            {
+                return a.ToString();
+            }
+
+            return Math.Tan(b).ToString();
+        }
+
+        public string Cotangens(string a)
+        {
+            if (!Double.TryParse(a, out double b))
+            {
+                return a.ToString();
+            }
+
+            return (1 / Math.Tan(b)).ToString();
+        }
+
+        public string SinusH(string a)
+        {
+            if (!Double.TryParse(a, out double b))
+            {
+                return a.ToString();
+            }
+
+            return Math.Sinh(b).ToString();
+        }
+
+        public string CosinusH(string a)
+        {
+            if (!Double.TryParse(a, out double b))
+            {
+                return a.ToString();
+            }
+
+            return Math.Cosh(b).ToString();
+        }
+
+        public string Log10(string a)
+        {
+            if (!Double.TryParse(a, out double b))
+            {
+                return a.ToString();
+            }
+
+            return Math.Log10(b).ToString();
+        }
+
+        public string Ln(string a)
+        {
+            if (!Double.TryParse(a, out double b))
+            {
+                return a.ToString();
+            }
+
+            return Math.Log(b).ToString();
+        }
+    }
+}
---
+++ Kalkulator/KalkulatorTests/OperacjeMatematyczneTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kalkulator.Tests
+{
+    [TestClass()]
+    public class OperacjeMatematyczneTests
+    {
+        OperacjeMatematyczne op = new OperacjeMatematyczne();
+
+        [TestMethod()]
+        public void DodajTest()
+        {
+            Assert.AreEqual("5", op.Dodaj(2, "3"));
+            Assert.AreEqual("0,75", op.Dodaj(0.5, "0,25"));
+            Assert.AreEqual("5", op.Dodaj(5, "*1"));
+        }
+
+        [TestMethod()]
+        public void OdejmijTest()
+        {
+            Assert.AreEqual("2", op.Odejmij(5, "3"));
+            Assert.AreEqual("3", op.Odejmij(1, "-2"));
+            Assert.AreEqual("5", op.Odejmij(5, "*1"));
+            Assert.AreEqual("0,5", op.Odejmij(0.75, "0,25"));
+        }
+
+        [TestMethod()]
+        public void MnozenieTest()
+        {
+            Assert.AreEqual("56", op.Mnozenie(8, "7"));
+            Assert.AreEqual("1", op.Mnozenie(0.1, "10"));
+            Assert.AreEqual("5", op.Mnozenie(5, "*2"));
+        }
+
+        [TestMethod()]
+        public void DzielenieTest()
+        {
+            Assert.AreEqual("0,5", op.Dzielenie(2, "4"));
+            Assert.AreEqual("9,8", op.Dzielenie(98, "10"));
+            Assert.AreEqual("5", op.Dzielenie(5, "*1"));
+        }
+
+        [TestMethod()]
+        public void PotegaTest()
+        {
+            Assert.AreEqual("32", op.Potega(2, "5"));
+            Assert.AreEqual("125", op.Potega(5, "3"));
+            Assert.AreEqual("1", op.Potega(1, "~5"));
+        }
+
+        [TestMethod()]
+        public void ProcentTest()
+        {
+            Assert.AreEqual("50,00%", op.Procent(2, "4"));
+            Assert.AreEqual("33,33%", op.Procent(1, "3"));
+            Assert.AreEqual("0", op.Procent(0, "!1"));
+        }
+
+        [TestMethod()]
+        public void PierwiastekTest()
+        {
+            for(double i = 1; i < 50; i += 0.5)
+                Assert.AreEqual(i.ToString(), op.Pierwiastek(i*i));
+        }
+
+        [TestMethod()]
+        public void ModuloTest()
+        {
+            Assert.AreEqual("1", op.Modulo(16, "15"));
+            Assert.AreEqual("5", op.Modulo(20, "15"));
+            Assert.AreEqual("5", op.Modulo(5, "!5"));
+        }
+
+        [TestMethod()]
+        public void BinarnieTest()
+        {
+            Assert.AreEqual("101", op.Binary("5"));
+            Assert.AreEqual("0", op.Binary("!123"));
+        }
+
+        [TestMethod()]
+        public void SilniaTest()
+        {
+            Assert.AreEqual("6", op.Silnia("3"));
+            Assert.AreEqual("1", op.Silnia("!3"));
+        }
+    }
+}
